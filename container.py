d2b98157044d6ccec99035ed2f66ff2b9d1b4c4d
smc181002 <meherchaitanya18802@gmail.com>
2020-11-07 11:59:50 +0530

    ssh support for docker

--- container.py
+++ container.py
@@ -12,7 +12,7 @@ command to be run ->
 docker run -dit --name _cname_ --net {_netname_ || brigde} 
         -v _volname_:_volmount_ _imgname_:_imgv_
 '''
-def start_container():
+def startContainer(sshIp=""):
     os.system("tput clear")
     imgname = input("enter docker image name (*req): ")
     imgv = input("enter docker image version (latest): ") or "latest"
@@ -30,7 +30,18 @@ def start_container():
     if (volname and volpath):
         volmnt = f"-v {volname}:{volpath}"
     
-    return subprocess.getoutput(f'sudo podman run -dit --net {netname} {volmnt} {cname} {imgname}:{imgv}')
+    return subprocess.getoutput(f'{sshIp} sudo podman run -dit --net {netname} {volmnt} {cname} {imgname}:{imgv}')
+
+def pullImg(sshIp=""): 
+    imgname = input("enter docker image name (*req): ")
+    imgv = input("enter docker image version (latest): ") or "latest"
+    
+    return subprocess.getoutput(f'{sshIp} sudo podman pull {imgname}:{imgv}')
+
+
+def status(resourceType, sshIp=""):
+    return subprocess.getoutput(f'{sshIp} sudo podman {resourceType} ls')
 
 if __name__ == '__main__':
     print("this code is not meant to be run.\nThis is a module")
+
--- main.py
+++ main.py
@@ -9,12 +9,16 @@ if __name__ == "__main__":
     [2] run hadoop
     [q] quit
     Enter your option: '''
+# TODO: options from 5
     docker = '''
     [1] create container
     [2] pull image
-    [3] create volume
-    [4] create network
-    [5] check status
+    [3] list running contianers
+    [4] list images
+    [5] start container
+    [6] stop container
+    [7] terminate or delete contianer
+    [8] search for images
     [b] back to main menu
     Enter your option: '''
     sshIp = ""
@@ -33,6 +37,7 @@ if __name__ == "__main__":
 
         if isSsh == "yes":
             sshIp = input('Enter the ssh IP or domain: ')
+            sshIp = f"ssh {sshIp} "
      
         toolOpt = input(text)
 
@@ -43,12 +48,20 @@ if __name__ == "__main__":
         elif toolOpt == "1":
             os.system("tput clear")
             subOpt = input(docker)
+            out=""
+            os.system("tput reset")
             if subOpt == "1":
-                out=container.start_container()
-                print(out)
-                input("press any key to exit")
+                out=container.startContainer(sshIp)
+            elif subOpt == "2":
+                out=container.pullImg(sshIp)
+            elif subOpt == "3":
+                out=container.status("container", sshIp)
+            elif subOpt == "4":
+                out=container.status("image", sshIp)
             else: 
                 continue
+            print(out)
+            input("press any key to exit")
         else:
             continue
 
